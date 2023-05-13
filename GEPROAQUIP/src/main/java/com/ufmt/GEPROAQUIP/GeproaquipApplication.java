1f5a3e65539aa46cf13def5dbe212058ef45cb50
jogodoy8 <joelmagodoy8@gmail.com>
2023-05-13 13:35:03 -0400

    Iniciando o projeto GEPROAQUIP

---
+++ GEPROAQUIP/src/main/java/com/ufmt/GEPROAQUIP/GeproaquipApplication.java
@@ -0,0 +1,13 @@
+package com.ufmt.GEPROAQUIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeproaquipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeproaquipApplication.class, args);
+	}
+
+}
---
+++ GEPROAQUIP/src/test/java/com/ufmt/GEPROAQUIP/GeproaquipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufmt.GEPROAQUIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeproaquipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
