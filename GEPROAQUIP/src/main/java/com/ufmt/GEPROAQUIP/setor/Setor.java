3abef54ea2ad383cf73f8fe029b8afef0fb2aaad
jogodoy8 <joelmagodoy8@gmail.com>
2023-05-13 15:41:15 -0400

    Criando as instancias setor e unidade do ProjetoGEPROAQUIP

---
+++ GEPROAQUIP/src/main/java/com/ufmt/GEPROAQUIP/setor/Setor.java
@@ -0,0 +1,34 @@
+package com.ufmt.GEPROAQUIP.setor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.ufmt.GEPROAQUIP.unidade.Unidade;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "setor")
+@SequenceGenerator(name = "seqSetor", sequenceName = "seq_setor_id", allocationSize = 1)
+@Getter
+@Setter
+public class Setor {
+    @Id
+    @GeneratedValue(generator = "seqSetor", strategy = GenerationType.SEQUENCE)
+    private int id;
+    @Column
+    private String nome;
+    @Column
+    private String descricao;
+    @ManyToOne
+    @JoinColumn(name = "unidade_id")
+    private Unidade unidade_id;
+}
---
+++ GEPROAQUIP/src/main/java/com/ufmt/GEPROAQUIP/unidade/Unidade.java
@@ -0,0 +1,31 @@
+package com.ufmt.GEPROAQUIP.unidade;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.ufmt.GEPROAQUIP.setor.Setor;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+ @Table(name = "unidade")
+ @SequenceGenerator(name = "seqUnidade", sequenceName = "seq_unidade_id", allocationSize = 1)
+ @Getter
+ @Setter
+
+public class Unidade {
+
+    @Id
+    @GeneratedValue(generator = "seqUnidade", strategy = GenerationType.SEQUENCE)
+    private int id;
+    @OneToMany(mappedBy = "unidade_id")
+    private List<Setor> setores;
+ }
